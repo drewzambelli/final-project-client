3c2f8799379d9eae57558e03441507ed7e6c86bc
drewzambelli <drewzambelli@gmail.com>
2024-11-16 23:18:18 -0500

    focusing on single student view delete/edit

--- src/components/views/StudentView.js
+++ src/components/views/StudentView.js
@@ -4,17 +4,45 @@ StudentView.js
 The Views component is responsible for rendering web page with data provided by the corresponding Container component.
 It constructs a React component to display the single student view page.
 ================================================== */
+import React from 'react';
+import {Link, useHistory } from 'react-router-dom';
+import PropTypes from "prop-types";
+
 const StudentView = (props) => {
-  const { student } = props;
+  const { student, deleteStudent } = props;
+  const history = useHistory();
+
+  const handleDelete = () => {
+    deleteStudent(student.id);
+    history.push('/students');
+  }
 
   // Render a single Student view 
   return (
     <div>
       <h1>{student.firstname + " " + student.lastname}</h1>
       <h3>{student.campus.name}</h3>
+
+      <p><strong>Age:</strong> {student.age}</p>
+      <p><strong>Year:</strong> {student.yearInSchool}</p>
+      <p><strong>Email:</strong> {student.email}</p>
+      <p><strong>Address:</strong> {student.address}</p>
+
+      {/* Added Edit Button */}
+      <Link to={`/students/edit/${student.id}`} style={{ marginRight: '10px' }}>
+        <button>Edit Student</button>
+      </Link>
+
+      {/* delete button */}
+      <button onClick={handleDelete}>Delete Student</button>
+
     </div>
   );
 
+};
+
+StudentView.propTypes = {
+  deleteStudent: PropTypes.func.isRequired,
 };
 
 export default StudentView;
